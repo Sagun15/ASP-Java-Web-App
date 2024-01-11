012ea8ee80d848589c19fa7fef6faa600f952872
Sagun Sangwan <sagun.sangwan@kickdrumtech.com>
2024-01-12 01:30:30 +0530

    Added an endpoint to log message

--- src/main/java/com/appservice/webapp/WebappApplication.java
+++ src/main/java/com/appservice/webapp/WebappApplication.java
@@ -2,6 +2,8 @@ package com.appservice.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WebappApplication {
@@ -10,4 +12,11 @@ public class WebappApplication {
 		SpringApplication.run(WebappApplication.class, args);
 	}
 
+	@RestController
+	class HelloController {
+		@GetMapping("/")
+		public String index() {
+			return "Hello, Azure!";
+		}
+	}
 }
